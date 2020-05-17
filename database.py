3fe6f304d807fabae4d9ba2d082ae9d373d7da81
dsizo <dsizov1999@mail.ru>
2020-05-17 15:25:25 +0300

    itterable error

--- database.py
+++ database.py
@@ -89,7 +89,7 @@ class DBCommands:
     async def list_hw(self):
         user_id = types.User.get_current().id
         homework_list = await HW.select('title').where(HW.id == 1).gino().scalar()
-        return homework_list
+        return str(homework_list)
 
 
 async def create_db():
