052199f9640ffe5768c9a411c40d423c4a0887b3
dsizo <dsizov1999@mail.ru>
2020-05-17 23:10:23 +0300

    unmaked homework list

--- handlers.py
+++ handlers.py
@@ -29,7 +29,7 @@ async def my_hw(message: types.Message):
     chat_id = message.from_user.id
     all_unmade_id = await db.done_unmade()
     for num, done in enumerate(all_unmade_id):
-        current_hw = db.get_hw(done.homework_id)
+        current_hw = await db.get_hw(done.homework_id)
         for num1, hw in enumerate(current_hw):
             text = f"<b>ДЗ</b> \t№{hw.id}: <u>{hw.title}</u>\n<b>Описание:</b> {hw.description}\n"
             await bot.send_message(chat_id, text)
