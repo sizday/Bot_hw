07fc9b995462a5683b901198534c70647898d1b9
dsizo <dsizov1999@mail.ru>
2020-05-18 14:39:17 +0300

    unmaked list

--- database.py
+++ database.py
@@ -96,7 +96,7 @@ class DBCommands:
     async def done_unmade(self):
         user_id = types.User.get_current().id
         user = await self.get_user(user_id)
-        done_unmade_list = await Done.query.where(Done.successful).gino.all()
+        done_unmade_list = await Done.query.where(not Done.successful).gino.all()
         return done_unmade_list
 
     async def get_hw(self, homework_id) -> HW:
