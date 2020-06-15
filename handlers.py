76650d811224f46ed23059acfe6b8ab6289c73d3
dsizo <dsizov1999@mail.ru>
2020-06-15 18:34:07 +0300

    bot always rate on 5

--- handlers.py
+++ handlers.py
@@ -77,11 +77,10 @@ async def enter_price(message: Message, state: FSMContext):
     await message.answer('ДЗ успешно отправлено')
     hw = await db.get_hw(done.homework_id)
     if hw.type == 'Test':
-        await message.answer(f'{hw.answer}\n{done.answer}')
         answer = bot.get_file(file_id=hw.answer)
         test = bot.get_file(file_id=done.answer)
-        answer.download(f'./{hw.answer}')
-        test.download(f'./{done.answer}')
+        for num, w in enumerate(answer):
+            await message.answer(f'{w}')
         result = open_file(hw.answer, done.answer)
         for answer in result[1]:
             await message.answer(answer)
