5bf619cae4d8fdc80fb331a4778ae4da00e3304b
dsizo <dsizov1999@mail.ru>
2020-06-20 23:51:21 +0300

    add python program functional

---
+++ python_check.py
@@ -0,0 +1,15 @@
+import subprocess
+
+
+def compare_files(answer_file, program_file):
+    answer_text = str(answer_file.getvalue(), 'utf-8')
+    temp_origin = "my_program.py"
+    with open(temp_origin, 'wb') as original_file:
+        original_file.write(program_file.read())
+    program = "python my_program.py"
+    process = subprocess.Popen(program, stdout=subprocess.PIPE, encoding='utf-8')
+    data_text = process.communicate()[0]
+    if answer_text == data_text:
+        return 5
+    else:
+        return 2
