51fc131f39ca38f92fb556a0506d72043bf64a68
dsizo <dsizov1999@mail.ru>
2020-06-21 00:28:49 +0300

    add python program functional

--- python_check.py
+++ python_check.py
@@ -7,12 +7,9 @@ def compare_files(answer_file, program_file):
     temp_origin = "my_program.py"
     with open(temp_origin, 'wb') as original_file:
         original_file.write(program_file.read())
-    """
     program = "python my_program.py"
-    process = subprocess.Popen(program, stdout=subprocess.PIPE, encoding='utf-8')
-    data_text = process.communicate()[0]
-    """
-    data_text = os.system("python my_program.py")
+    data_text = subprocess.check_output(program, encoding='utf-8')
+    # data_text = os.system("python my_program.py")
     os.remove(temp_origin)
     if answer_text == data_text:
         return 5
