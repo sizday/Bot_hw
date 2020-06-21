5099f8e2a7f6697070a3eac49ad73b358b0c73de
dsizo <dsizov1999@mail.ru>
2020-06-21 23:37:56 +0300

    my_marks and rating function

--- handlers.py
+++ handlers.py
@@ -108,6 +108,7 @@ async def my_marks(message: Message):
     if not all_marks:
         await message.answer('У вас нет оценок')
     else:
+        await message.answer(all_marks)
         text_marks = ' '.join(all_marks)
         mean_marks = round(sum(all_marks)/len(all_marks), 2)
         await message.answer(f'Ваши оценки:\n{text_marks}\nСредний балл = {mean_marks}')
