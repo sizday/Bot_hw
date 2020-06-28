e4992ae37f9fec0e118b361a0c83e864dd07d944
dsizo <dsizov1999@mail.ru>
2020-06-28 15:25:22 +0300

    + admin

---
+++ admin.py
@@ -0,0 +1,12 @@
+from config import admin_id
+
+
+class Admin:
+    id = admin_id
+    teachers = [admin_id]
+
+    def add_teacher(self, teacher_id: int):
+        self.teachers.append(teacher_id)
+
+    def delete_teacher(self, teacher_id: int):
+        self.teachers.remove(teacher_id)
